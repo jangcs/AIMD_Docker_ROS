45d366494c830edc0a1b78713b2ad52ed4ae7f58
Choulsoo Jang <jangcs@gmail.com>
2025-05-22 14:00:50 +0900

    K8s deployment shell scrcipt sample

--- k8s_ros.sh
+++ k8s_ros.sh
@@ -2,3 +2,4 @@ export CYCLONEDDS_URI=`pwd`/cyclonedds.xml
 kubectl apply -f k8s_pubsub.yaml
 pod=`kubectl get pods | grep ros-pubsub | head -n 1 | awk '{print $1}'`
 kubectl logs --follow $pod -c ros-sub
+
